c355e2390dd3d077c829c467bf2b75d50eb00486
Deep Majumder <deep.majumder2019@gmail.com>
2020-08-07 10:31:03 +0530

    Added writing of predicates

--- src/domain.ts
+++ src/domain.ts
@@ -2,34 +2,34 @@ import * as fs from 'fs';
 import { Puzzle } from './parser';
 
 function writeMoveAction(path: string, direction: string, length: number) {
-  const actionName = `move-${direction}-${length}`;
+  const actionName = `move_${direction}_${length}`;
   fs.writeFileSync(path, `(:action ${actionName}\n`, { flag: 'a' });
 
   fs.writeFileSync(path, ':parameters (?car)\n', { flag: 'a' });
 
-  let precondition = ':precondition (and (is-car ?car)\n';
+  let precondition = ':precondition (and (is_car ?car)\n';
   if (direction === 'left' || direction === 'right') {
-    precondition += '(is-horizontal ?car)\n';
+    precondition += '(is_horizontal ?car)\n';
   } else {
-    precondition += '(is-vertical ?car)\n';
+    precondition += '(is_vertical ?car)\n';
   }
   precondition += '(forall (?sq1 ?sq2 ?car2) (and ';
-  precondition += '(is-car ?car2)\n';
-  precondition += '(is-sq ?sq1)\n';
-  precondition += '(is-sq ?sq2)\n';
-  precondition += '(in-car ?car ?sq1)\n';
-  precondition += '(not (in-car ?car2 ?sq2))\n';
-  precondition += `(in-${direction}-dist-${length} ?sq1 ?sq2))))\n`;
+  precondition += '(is_car ?car2)\n';
+  precondition += '(is_sq ?sq1)\n';
+  precondition += '(is_sq ?sq2)\n';
+  precondition += '(in_car ?car ?sq1)\n';
+  precondition += '(not (in_car ?car2 ?sq2))\n';
+  precondition += `(in_${direction}_dist_${length} ?sq1 ?sq2))))\n`;
   fs.writeFileSync(path, precondition, { flag: 'a' });
 
   let effect = ':effect (forall (?sq1 ?sq2) (when (and ';
-  effect += '(is-sq ?sq1)\n';
-  effect += '(is-sq ?sq2)\n';
-  effect += '(in-car ?car ?sq1)\n';
-  effect += `(in-${direction}-dist-${length} ?sq1 ?sq2)))\n`;
+  effect += '(is_sq ?sq1)\n';
+  effect += '(is_sq ?sq2)\n';
+  effect += '(in_car ?car ?sq1)\n';
+  effect += `(in_${direction}_dist_${length} ?sq1 ?sq2))\n`;
   effect += '(and ';
-  effect += '(not (in-car ?car ?sq1))\n';
-  effect += '(in-car ?car ?sq2)))\n';
+  effect += '(not (in_car ?car ?sq1))\n';
+  effect += '(in_car ?car ?sq2))))\n';
   fs.writeFileSync(path, effect, { flag: 'a' });
 
   fs.writeFileSync(path, ')\n\n', { flag: 'a' });
@@ -53,7 +53,29 @@ function writeMoves(path: string, puzzle: Puzzle) {
 }
 
 function writePredicates(path: string, puzzle: Puzzle) {
+  fs.writeFileSync(path, '(:predicates ', { flag : 'a' });
+  fs.writeFileSync(path, '(is_sq ?sq)\n', { flag: 'a' });
+  fs.writeFileSync(path, '(is_car ?car)\n', { flag: 'a' });
+  fs.writeFileSync(path, '(in_car ?car ?sq)\n', { flag: 'a' });
+  fs.writeFileSync(path, '(is_vertical ?car)\n', { flag: 'a' });
+  fs.writeFileSync(path, '(is_horizontal ?car)\n', { flag: 'a' });
 
+  const directions = ['right', 'left', 'up', 'down'];
+
+  for (let key in directions) {
+    let limit: number;
+    let direction = directions[key];
+    if (direction === 'left' || direction === 'right') {
+      limit = puzzle.columns;
+    } else {
+      limit = puzzle.rows;
+    }
+    for (let i = 1; i < limit; i++) {
+      fs.writeFileSync(path, `(in_${direction}_dist_${i} ?sq1 ?sq2)\n`, { flag: 'a' });
+    }
+  }
+
+  fs.writeFileSync(path, ')\n', { flag: 'a' });
 }
 
 export function writeDomain(puzzle: Puzzle) {
@@ -63,8 +85,9 @@ export function writeDomain(puzzle: Puzzle) {
 
   fs.writeFileSync(path, '(define (domain BLOCK_PUZZLE)\n(:requirements :adl)\n');
 
-  writeMoves(path, puzzle);
   writePredicates(path, puzzle);
+  writeMoves(path, puzzle);
+  
 
   fs.writeFileSync(path, ')\n', { flag: 'a' });
 }
